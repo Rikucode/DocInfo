ea916545393719358e13788a6c6d5c263f883b6a
Rikucode <pavlyu4enko.a@gmail.com>
2022-09-30 14:26:44 +1000

    rework

---
+++ DomainLogic/Appointment.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainLogic
+{
+    internal class Appointment
+    {
+        DateTime appointmentStart;
+        DateTime appointmentEnd;
+        int patientId;
+        int doctorId;
+    }
+}
---
+++ DomainLogic/Speciality.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainLogic
+{
+    internal class Speciality
+    {
+        int id;
+        string name;
+    }
+}
